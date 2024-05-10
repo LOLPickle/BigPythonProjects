9d80d76a5a24a5bf3bf5271fb6fc3829938b0c76
LOLPickle <vitalijsvoboda11@gmail.com>
2024-05-10 14:30:08 +0300

    add bg color

--- Alien Invasion.py
+++ Alien Invasion.py
@@ -14,12 +14,18 @@ class AlienInvasion:
 		self.screen = pygame.dispaly.set_mode((1200, 800))
 		pygame.dispaly.set_caption("Alien Invasion")
 
+		#color bg
+		self.bg_color = (280, 230, 230)
+
 	def run_game(self):
 
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
+
+				#color bg
+				self.screen.fill(self.settings.bg_color)
 
 			pygame.display.flip()
 
