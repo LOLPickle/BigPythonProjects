cc7702677b77b5797c31ff673491be17080f2235
LOLPickle <vitalijsvoboda11@gmail.com>
2024-05-10 14:27:28 +0300

    first commit

---
+++ Alien Invasion.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+
+
+
+	def __init__(self):
+
+		pygame.init()
+
+
+		self.screen = pygame.dispaly.set_mode((1200, 800))
+		pygame.dispaly.set_caption("Alien Invasion")
+
+	def run_game(self):
+
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			pygame.display.flip()
+
+
+if __name__ == '__main__':
+	ai = AlienInvasion()
+	ai.run_game()
